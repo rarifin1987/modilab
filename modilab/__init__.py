0a2e4201604360ecb3ae17a285797795166c1abc
rarifin1987 <rarifin87@gmail.com>
2026-09-25 09:40:23 +0700

    Delete modilab directory

--- modilab/__init__.py
+++
@@ -1,6 +0,0 @@
-"""MoDiLab: a bilingual virtual laboratory for teaching molecular dynamics."""
-
-from .core import Parameters, Result, compute_forces, dimer, run
-
-__version__ = "0.3.1"
-__all__ = ["Parameters", "Result", "compute_forces", "dimer", "run"]
